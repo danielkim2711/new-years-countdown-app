042b986e567916a069070c8c00ea1cef1cd7c89f
Daniel Kim <danielkim2711@gmail.com>
2021-09-22 22:02:18 +1200

    Fix format

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const newYears = '31 Dec 2021';
+const newYears = '01 Jan 2022';
 
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
@@ -6,24 +6,24 @@ const minsEl = document.getElementById('mins');
 const secondsEl = document.getElementById('seconds');
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = hours;
-    minsEl.innerHTML = mins;
-    secondsEl.innerHTML = formatTime(seconds);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minsEl.innerHTML = mins;
+  secondsEl.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
+  return time < 10 ? `0${time}` : time;
 }
 
 countdown();
