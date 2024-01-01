489883f368aff8cc5421b9d90369207808b415f3
Daniel Kim <hyojun.kim@ezyvet.com>
2024-01-01 13:07:39 +1300

    refactor: update countdown

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-const newYears = '01 Jan 2024';
+const startNewYearCountdown = () => {
+  const { days, hours, mins, seconds } = calculateTimeUnits();
+  updateCountdown(days, hours, mins, seconds);
+};
 
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minsEl = document.getElementById('mins');
-const secondsEl = document.getElementById('seconds');
-
-function countdown() {
-  const newYearsDate = new Date(newYears);
+const calculateTimeUnits = () => {
+  const newYearsDate = new Date('01 Jan 2025');
   const currentDate = new Date();
 
   const totalSeconds = (newYearsDate - currentDate) / 1000;
@@ -16,22 +14,29 @@ function countdown() {
   const mins = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 
+  return { days, hours, mins, seconds };
+};
+
+const updateCountdown = (days, hours, mins, seconds) => {
+  const daysEl = document.getElementById('days');
+  const hoursEl = document.getElementById('hours');
+  const minsEl = document.getElementById('mins');
+  const secondsEl = document.getElementById('seconds');
+
   daysEl.innerHTML = formatDays(days);
   hoursEl.innerHTML = formatTime(hours);
   minsEl.innerHTML = formatTime(mins);
   secondsEl.innerHTML = formatTime(seconds);
-}
+};
 
-function formatDays(days) {
+const formatDays = (days) => {
   if (days < 100 && days >= 10) return `0${days}`;
   else if (days < 10) return `00${days}`;
-  else return days;
-}
 
-function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
-}
+  return days;
+};
 
-countdown();
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
-setInterval(countdown, 1000);
+startNewYearCountdown();
+setInterval(startNewYearCountdown, 1000);
