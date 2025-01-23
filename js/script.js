9de37b4537d7a1cf96e48c5f92886c503f4d9100
Daniel Kim <danielkim2711@gmail.com>
2025-01-23 22:01:40 +1300

    refactor: use constants and combine functions

--- js/script.js
+++ js/script.js
@@ -8,14 +8,15 @@ const getTimeUnits = (futureDate) => {
   const currentDate = new Date();
   const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  const oneDay = 86400;
-  const oneHour = 3600;
+  const SECONDS_IN_A_DAY = 86400;
+  const SECONDS_IN_AN_HOUR = 3600;
+  const SECONDS_IN_A_MINUTE = 60;
 
   return {
-    days: Math.floor(totalSeconds / oneDay),
-    hours: Math.floor(totalSeconds / oneHour) % 24,
-    mins: Math.floor(totalSeconds / 60) % 60,
-    seconds: Math.floor(totalSeconds % 60),
+    days: Math.floor(totalSeconds / SECONDS_IN_A_DAY),
+    hours: Math.floor((totalSeconds % SECONDS_IN_A_DAY) / SECONDS_IN_AN_HOUR),
+    mins: Math.floor((totalSeconds % SECONDS_IN_AN_HOUR) / SECONDS_IN_A_MINUTE),
+    seconds: Math.floor(totalSeconds % SECONDS_IN_A_MINUTE),
   };
 };
 
@@ -25,14 +26,14 @@ const updateCountdownTimer = (days, hours, mins, seconds) => {
   const minsEl = document.getElementById('mins');
   const secondsEl = document.getElementById('seconds');
 
-  daysEl.textContent = formatDays(days);
-  hoursEl.textContent = formatTime(hours);
-  minsEl.textContent = formatTime(mins);
-  secondsEl.textContent = formatTime(seconds);
+  daysEl.textContent = formatNumberWithZeros(days, 3);
+  hoursEl.textContent = formatNumberWithZeros(hours, 2);
+  minsEl.textContent = formatNumberWithZeros(mins, 2);
+  secondsEl.textContent = formatNumberWithZeros(seconds, 2);
 };
 
-const formatDays = (days) => String(days).padStart(3, '0');
-const formatTime = (time) => String(time).padStart(2, '0');
+const formatNumberWithZeros = (num, length) =>
+  String(num).padStart(length, '0');
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
